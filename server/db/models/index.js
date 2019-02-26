458f53e393d20ed938bb13bb70df56c0e5d02877
Delilah Clement <delilahclement@gmail.com>
2019-02-26 15:21:07 -0600

    using seed data magic methods

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,17 +5,22 @@ const Product = require('./Product')
 const Order = require('./Order')
 const Category = require('./Category')
 const Review = require('./Review')
+const OrderedProducts = require('./OrderedProducts')
 
 User.belongsToMany(Address, {through: UserAddress})
 Address.belongsToMany(User, {through: UserAddress})
 
-Product.belongsToMany(Order, {through: 'OrderedProducts'})
-Order.belongsToMany(Product, {through: 'OrderedProducts'})
+Product.belongsToMany(Order, {through: OrderedProducts})
+Order.belongsToMany(Product, {through: OrderedProducts})
 
 Order.belongsTo(User)
 User.hasMany(Order)
 
 Product.hasMany(Category)
+
+Product.hasMany(Review)
+
+Review.belongsTo(User)
 
 module.exports = {
   User,
@@ -24,5 +29,6 @@ module.exports = {
   Order,
   Product,
   Category,
-  Review
+  Review,
+  OrderedProducts
 }
