f869a5eea088d13093964d51e3f7b6a4fc00ce30
jan <janusbased@gmail.com>
2019-02-26 16:25:55 -0600

    Feature: wrote basic user routes, which still need to authorize; also updated Product, User, and Address models.

--- server/db/models/Address.js
+++ server/db/models/Address.js
@@ -1,17 +1,24 @@
+/* eslint-disable camelcase */
 const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Address = db.define('address', {
   address_line1: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   address_line2: {
     type: Sequelize.STRING
   },
   city_province: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   postalCode: {
     type: Sequelize.INTEGER,
@@ -19,7 +26,10 @@ const Address = db.define('address', {
   },
   country: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   }
 })
 
