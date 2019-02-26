49e54875b259ae6deeebf26a13bd6d1cbbe62318
Delilah Clement <delilahclement@gmail.com>
2019-02-26 16:48:55 -0600

    created allproducts component with react-bootstrap

--- client/main.js
+++ client/main.js
@@ -2,13 +2,10 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, AllProducts} from './components'
 import {me} from './store'
 
-/**
- * COMPONENT
- */
-class Routes extends Component {
+class Main extends Component {
   componentDidMount() {
     this.props.loadInitialData()
   }
@@ -19,6 +16,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/products" component={AllProducts} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
@@ -34,9 +32,6 @@ class Routes extends Component {
   }
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
@@ -55,12 +50,12 @@ const mapDispatch = dispatch => {
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(connect(mapState, mapDispatch)(Main))
 
 /**
  * PROP TYPES
  */
-Routes.propTypes = {
+Main.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
 }
