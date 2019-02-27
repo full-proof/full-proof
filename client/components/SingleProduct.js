847eb92eee99f0be052be26ac492933c6d79d880
Delilah Clement <delilahclement@gmail.com>
2019-02-27 11:26:07 -0600

    Created SingleProduct component

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {fetchProductThunk} from '../store/product'
+
+export class SingleProduct extends React.Component {
+  componentDidMount() {
+    console.log('component did mount', this.props)
+    this.props.fetchProduct(this.props.match.params.id)
+  }
+
+  render() {
+    const product = this.props.singleProduct
+    console.log(this.props)
+    return (
+      <div>
+        <Card key={product.id} style={{width: '30rem'}}>
+          <Card.Img variant="top" src={product.imgUrl} />
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>{product.price}</ListGroupItem>
+            <ListGroupItem>{product.quantity}</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Place in Cart</Card.Link>
+            {/* {product.review.rating} */}
+          </Card.Body>
+        </Card>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    singleProduct: state.products.singleProduct
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProduct: id => dispatch(fetchProductThunk(id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
