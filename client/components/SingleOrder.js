fa1f11741064bfcfbb982bce7351e6ef1bab174d
jan <janusbased@gmail.com>
2019-02-28 12:43:14 -0600

    got the products to render in a table

--- client/components/SingleOrder.js
+++ client/components/SingleOrder.js
@@ -13,28 +13,53 @@ export class SingleOrder extends React.Component {
     const order = this.props.singleOrder.order || {}
     console.log('order', order)
     const productArray = this.props.singleOrder.products || []
+    console.log('products', productArray)
 
     return order.user ? (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Order Number</th>
-            <th>Status</th>
-            <th>Order Date</th>
-            <th>User</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            <tr key={order.id}>
-              <td>{order.id}</td>
-              <td>{order.status}</td>
-              <td>{order.createdAt}</td>
-              <td>{order.user.name}</td>
+      <div>
+        <h3>Order Info</h3>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>User</th>
+              <th>Status</th>
+              <th>Order Date</th>
             </tr>
-          }
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {
+              <tr key={order.id}>
+                <td>{order.id}</td>
+                <td>{order.user.name}</td>
+                <td>{order.status}</td>
+                <td>{order.createdAt}</td>
+              </tr>
+            }
+          </tbody>
+        </Table>
+        <h3>Products in Order</h3>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Product ID</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productArray.map(product => (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.title}</td>
+                <td>{product.price}</td>
+                <td>{product.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     ) : (
       <p>Loading</p>
     )
